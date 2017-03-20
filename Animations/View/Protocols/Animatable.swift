37095fcf970a1aa1e94044381ce7f39ea3cfe50c
Renaud Cousin <rcousin@octo.com>
2017-03-20 12:02:20 +0100

    Add ShareButton

---
+++ Animations/View/Protocols/Animatable.swift
@@ -0,0 +1,9 @@
+//
+//  Animatable.swift
+//  Animations
+//
+//  Created by AbsolutRenal on 20/03/2017.
+//  Copyright © 2017 AbsolutRenal. All rights reserved.
+//
+
+import Foundation
--- Animations/View/Share/ShareButton.swift
+++ Animations/View/Share/ShareButton.swift
@@ -9,6 +9,58 @@
 import Foundation
 import UIKit
 
-class ShareButton: UIView {
+protocol ShareButtonDelegate: class {
   
+}
+
+class ShareButton: UIView {
+  // *********************************************************************
+  // MARK: - Constants
+  private let shareLabel = "Share"
+  
+  // *********************************************************************
+  // MARK: - IBOutlet
+  @IBOutlet weak var whiteButton: UIButton! {
+    didSet {
+      whiteButton.titleLabel?.font = UIFont.systemFont(ofSize: 17.0, weight: UIFontWeightThin)
+      whiteButton.setTitle(shareLabel, for: .normal)
+      whiteButton.setTitleColor(UIColor(red: 193.0/255.0, green: 193.0/255.0, blue: 193.0/255.0, alpha: 1.0), for: .normal)
+      whiteButton.layer.cornerRadius = whiteButton.layer.bounds.height * 0.5
+    }
+  }
+  
+  // *********************************************************************
+  // MARK: - Properties
+  weak var delegate: ShareButtonDelegate?
+  
+  // *********************************************************************
+  // MARK: - Lifecycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    configure()
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    configure()
+  }
+  
+  // *********************************************************************
+  // MARK: - Private func
+  private func configure() {
+    layer.cornerRadius = layer.bounds.height * 0.5
+  }
+  
+  private func openBox() {
+    whiteButton.layer.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+    
+  }
+  
+  // *********************************************************************
+  // MARK: - IBAction
 }
