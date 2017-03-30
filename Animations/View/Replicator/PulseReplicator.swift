f4500cd56b98b3980e045f0db8e784dbd9027b4b
Renaud Cousin <rcousin@octo.com>
2017-03-30 12:16:24 +0200

    Remove rotation/translation and replace by scale only

--- Animations/View/Replicator/PulseReplicator.swift
+++ Animations/View/Replicator/PulseReplicator.swift
@@ -12,7 +12,7 @@ import UIKit
 class PulseReplicator: UIView, Animatable {
   // *********************************************************************
   // MARK: - Constants
-  private let particuleSize = 20
+  private let particuleSize = 120
   private let rotationCount = 65
   private let duplicateCount = 12
   private let growEasing = CAMediaTimingFunction(controlPoints: 0.6, 0, 0.6, 1.2)
@@ -44,7 +44,7 @@ class PulseReplicator: UIView, Animatable {
   // MARK: - Private
   private func configure() {
     create()
-    addPerspective()
+//    addPerspective()
   }
   
   private func create() {
@@ -65,39 +65,22 @@ class PulseReplicator: UIView, Animatable {
                   forKey: "pulse")
     layer.addSublayer(particule)
     
-    let translateReplicator = CAReplicatorLayer()
-    translateReplicator.instanceBlueOffset = -0.1
-    translateReplicator.instanceRedOffset = -0.1
-    translateReplicator.instanceGreenOffset = -0.1
-    translateReplicator.instanceCount = duplicateCount
-    translateReplicator.instanceDelay = 0.1
-    translateReplicator.instanceTransform = CATransform3DTranslate(CATransform3DIdentity,
-                                                                   CGFloat(particuleSize),
-                                                                   0,
-                                                                   0)
-    translateReplicator.addSublayer(particule)
-    translateReplicator.bounds = CGRect(x: (layer.bounds.size.width - CGFloat(particuleSize)) * 0.5,
-                                        y: (layer.bounds.size.height - CGFloat(particuleSize)) * 0.5,
-                                        width: layer.bounds.size.width * 0.8,
-                                        height: CGFloat(particuleSize))
-    translateReplicator.masksToBounds = true
-    let anchorX = (CGFloat(particuleSize) * 0.5) / translateReplicator.bounds.size.width
-    translateReplicator.anchorPoint = CGPoint(x: anchorX,
-                                              y: 0.5)
-    
-    let rotateReplicator = CAReplicatorLayer()
-    rotateReplicator.instanceCount = rotationCount
-    rotateReplicator.instanceTransform = CATransform3DRotate(CATransform3DIdentity,
-                                                             CGFloat(2 * Double.pi / Double(rotationCount)),
-                                                             0,
-                                                             0,
-                                                             1)
-    rotateReplicator.addSublayer(translateReplicator)
-    rotateReplicator.position = CGPoint(x: layer.bounds.size.width * 0.5,
-                                        y: layer.bounds.size.height * 0.5)
-    rotateReplicator.shadowRadius = 1.0
-    rotateReplicator.shadowOpacity = 0.3
-    layer.addSublayer(rotateReplicator)
+    let scaleReplicator = CAReplicatorLayer()
+    scaleReplicator.instanceBlueOffset = -0.1
+    scaleReplicator.instanceRedOffset = -0.1
+    scaleReplicator.instanceGreenOffset = -0.1
+    scaleReplicator.instanceCount = 60
+    scaleReplicator.instanceDelay = 0.1
+    scaleReplicator.bounds = layer.bounds
+    scaleReplicator.position = particule.position
+    scaleReplicator.instanceTransform = CATransform3DScale(CATransform3DIdentity,
+                                                           0.9,
+                                                           0.9,
+                                                           1.0)
+    scaleReplicator.addSublayer(particule)
+    scaleReplicator.shadowRadius = 1.0
+    scaleReplicator.shadowOpacity = 0.3
+    layer.addSublayer(scaleReplicator)
   }
   
   private func addPerspective() {
@@ -113,14 +96,20 @@ class PulseReplicator: UIView, Animatable {
                                       duration: 0.8,
                                       delegate: nil,
                                       timingFunctions: [growEasing])
-    let depth = buildKeyFrameAnimation(keyPath: "zPosition",
-                                       values: [1000.0, -1000.0],
-                                       keyTimes: [0.0, 0.4],
-                                       duration: 0.8,
+//    let depth = buildKeyFrameAnimation(keyPath: "zPosition",
+//                                       values: [1000.0, -1000.0],
+//                                       keyTimes: [0.0, 0.4],
+//                                       duration: 0.8,
+//                                       delegate: nil,
+//                                       timingFunctions: [growEasing])
+    let color = buildKeyFrameAnimation(keyPath: "backgroundColor",
+                                       values: [UIColor.lightGray.cgColor, UIColor.red.cgColor],
+                                       keyTimes: [0.0, 1.0],
+                                       duration: 1.0,
                                        delegate: nil,
                                        timingFunctions: [growEasing])
     
-    let anim = buildAnimationGroup(animations: [grow, depth],
+    let anim = buildAnimationGroup(animations: [grow, color],
                                    duration: 0.8,
                                    delegate: nil)
     anim.autoreverses = true
