c28240d30236141e9644e90b5ef68c1b88c2c65e
Renaud Cousin <rcousin@octo.com>
2018-12-05 12:57:39 +0100

    clean classes in different files

---
+++ Animations/View/WWDC_CustomTransition/WWDCTransitionProperties.swift
@@ -0,0 +1,14 @@
+//
+//  WWDCTransitionProperties.swift
+//  Animations
+//
+//  Created by AbsolutRenal on 05/12/2018.
+//  Copyright © 2018 AbsolutRenal. All rights reserved.
+//
+
+import UIKit
+
+struct WWDCTransitionProperties {
+  let cornerRadius: CGFloat
+  let frame: CGRect
+}
---
+++ Animations/View/WWDC_CustomTransition/WWDCTransitioningDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  WWDCTransitioningDelegate.swift
+//  Animations
+//
+//  Created by AbsolutRenal on 05/12/2018.
+//  Copyright © 2018 AbsolutRenal. All rights reserved.
+//
+
+import UIKit
+
+final class WWDCTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
+  func animationController(forPresented presented: UIViewController,
+                           presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    return WWDCAnimatedTransitioning(from: source, dismissing: false)
+  }
+  
+  func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    return nil
+    //    return WWDCAnimatedTransitioning(dismissing: true)
+  }
+}
